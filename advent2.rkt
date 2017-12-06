8d94796169b73785fa4f3703104667c7e5f2a812
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2017-12-06 01:21:58 -0500

    Finished 2-2

--- advent2.rkt
+++ advent2.rkt
@@ -2,7 +2,8 @@
 
 ; A Spreadsheet is a [List-of [List-of Number] ]
 
-(define EX (list (list 5 1 9 5) (list 7 5 3) (list 2 4 6 8)))
+(define EX1 (list (list 5 1 9 5) (list 7 5 3) (list 2 4 6 8)))
+(define EX2 (list (list 5 9 2 8) (list 9 4 7 3) (list 3 8 6 5)))
 
 (define INPUT (map
                (lambda (l) (map string->number l))
@@ -12,3 +13,22 @@
   (local ((define (row-dif row)
             (- (apply max row) (apply min row))))
     (foldr + 0 (map row-dif ss))))
+
+;;; <rant> I am so mad. I took hours coding this function and I tried five different approaches
+;;; because I DIDN'T NOTICE ITS TEST DATA WERE DIFFERENT!!!
+;;; Anyways this might not be the cleanest approach I tried but I'm leaving it this way! </rant>
+(define (ss-divsum ss)
+  (local ((define (row-div init-row)
+            (let row-div-helper ([row init-row])
+              (local ((define (div-by-smth num l)
+                        (cond
+                          [(empty? l) #f]
+                          [(exact-integer? (/ num (first l))) (/ num (first l))]
+                          [else (div-by-smth num (rest l))])))
+                (cond
+                  [(empty? row) 0]
+                  [(boolean? (div-by-smth (first row) (remove (first row) init-row)))
+                   (row-div-helper (rest row))]
+                  [else (div-by-smth (first row) (remove (first row) init-row))])))))
+              
+    (foldr + 0 (map row-div ss))))
