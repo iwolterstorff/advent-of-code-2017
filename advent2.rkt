896f82ec784b4c0dc3db826f09cc024a400ab69e
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2017-12-05 22:51:38 -0500

    2-1 done

--- advent2.rkt
+++ advent2.rkt
@@ -4,14 +4,9 @@
 
 (define EX (list (list 5 1 9 5) (list 7 5 3) (list 2 4 6 8)))
 
-(define PORT-IN (open-input-file "advent2-in.txt"))
-(define INPUT (for/list ([num (stop-before PORT-IN (lambda (in) (eq? in "\t")))])
-                num))
-
-#;(define INPUT (for/list ([num (stop-before "5048	177	5280	5058"
-                                           char-whitespace?)])
-                num))
-
+(define INPUT (map
+               (lambda (l) (map string->number l))
+               (map string-split (file->lines "advent2-in.txt"))))
 
 (define (ss-checksum ss)
   (local ((define (row-dif row)
